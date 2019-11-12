1d9c1024d6eb0175972e3fba446144cd2e4f794a
nataliagullon <nataliagullon96@gmail.com>
2019-11-12 10:22:56 +0100

    filter people enwiki cluster part ready

--- dataset/filter_people_enwiki.py
+++ dataset/filter_people_enwiki.py
@@ -8,37 +8,45 @@ import json
 spark = SparkSession.builder.getOrCreate()
 sc = spark.sparkContext
 
-ENWIKI = "enwiki_overview.json"
-WIKIDATA_PEOPLE = "people_wikidata_sample.json"
+LOCAL_PATH = "hdfs:///user/gullon/"
+# LOCAL_PATH = "../data/"
+
+# WIKIDATA_PEOPLE = "people_wikidata_sample.json"
+WIKIDATA_PEOPLE = "people_wikidata.json"
+
+ENWIKI = "hdfs:///datasets/enwiki-20191001/enwiki-20191001-pages-articles-multistream.xml"
+# ENWIKI = "../data/enwiki_5000_lines.xml"
 
 # absolute path of the file - CHANGE WHEN RUN LOCAL
-PATH = "/media/sofia/DATA/EPFL/Applied Data Analysis/Project/wiki-gender/data/"
-
-# absolute path of the file - CHANGE WHEN RUN LOCAL
-file = os.path.join(PATH, "enwiki_5000_lines.xml")
 df = spark.read.format("com.databricks.spark.xml") \
 				.options(rowTag="page") \
-				.load(file)
+				.load(ENWIKI)
 
-print(df.show())
+# print(df.show())
 print(df.printSchema())
 
 df = df.filter("redirect._title is null")
 df_enwiki_title = df.select("title", "revision.text._VALUE").toDF("title", "text")
-print(df_enwiki_title.show())
+# print(df_enwiki_title.show())
+print("================================")
+print("enwiki tite and text done")
+print("================================")
+
 
 # read the wikidata codes
-df_wikidata = spark.read.json(PATH + WIKIDATA_PEOPLE) 
-print(df_wikidata.show())
-
+df_wikidata = spark.read.json(LOCAL_PATH + WIKIDATA_PEOPLE)
+# print(df_wikidata.show())
 
 df_biographies = df_wikidata.join(df_enwiki_title,['title'],how='inner')
 
+print("================================")
+print("enwiki and wikidata join done")
+print("================================")
 
-print(df_biographies.show())
+# print(df_biographies.show())
 
 # save the df
-people_df.write.mode('overwrite').json(PATH + "people_wikidata.json")
+people_df.write.mode('overwrite').json(LOCAL_PATH + "biographies_wikipedia.json")
 
 # woohoo!
 print("!!!!!!!!!!!!!!!")
