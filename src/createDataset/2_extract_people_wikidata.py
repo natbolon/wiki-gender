0dd15656fb00a0e22be4c6489cd9cf146da09fde
s-kypr <sofia.kyp273@gmail.com>
2019-11-18 12:42:28 +0100

    refactorgit status!

---
+++ src/createDataset/2_extract_people_wikidata.py
@@ -0,0 +1,103 @@
+import os
+from pyspark.sql import *
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from datetime import datetime
+import json
+from params import *
+
+
+def only_enwiki(line): 
+	d = json.loads(line._VALUE)
+	try:
+		sl = d['sitelinks']
+		l = d['labels']
+		if "enwiki" in sl and "en" in l:
+			return True
+		else:
+			return False
+	except KeyError: 
+		return False
+
+
+def main(**params):
+	params = dict(
+		**params
+	)
+
+	local = params['local']
+
+	if local:
+		LOCAL_PATH = "../../data/"
+		WIKI_DATA = os.path.join(LOCAL_PATH, "wikidata_5000_lines.xml")
+		QID_DATA = os.path.join(LOCAL_PATH, "qid_people_wikidata.csv")
+
+	else: # running in the cluster
+		LOCAL_PATH = "hdfs:///user/gullon/"
+		WIKI_DATA = "hdfs:///datasets/wikidatawiki/wikidatawiki-20170301-pages-articles-multistream.xml"
+		QID_DATA = os.path.join(LOCAL_PATH, "qid_people_wikidata.csv")
+
+	spark = SparkSession.builder.getOrCreate()
+	sc = spark.sparkContext
+
+	# read the json with the info on the people
+	df = spark.read.format("com.databricks.spark.xml") \
+					.options(rowTag="page") \
+					.load(WIKI_DATA)
+
+	# we want to access revision, text, _VALUE
+	df = df.select("revision.text._VALUE", "title")
+
+	# read the wikidata codes
+	qid_df = spark.read.csv(QID_DATA, header=True)
+
+	if local:
+		print(qid_df.show())
+
+	qid_df = qid_df.select("qid", "gender", "occupation").toDF("title", "gender", "occupation")
+
+	people_df = qid_df.join(df,['title'],how='inner')
+
+	people_df.printSchema()
+
+	if local:
+		print(people_df.show())
+	else:
+		print("="*50)
+		print("Wikidata filtered by Q code!")
+		print("="*50)
+
+	# people_df.write.mode('overwrite').json(os.path.join(LOCAL_PATH, "error.json"))
+
+	people_filtered = people_df.select("_VALUE").rdd.filter(only_enwiki)
+
+	name_wiki_df = spark.read.json(people_filtered.map(lambda r: r["_VALUE"]))
+
+	name_wiki_df = name_wiki_df.select("id", "labels.en.value", "sitelinks.enwiki.title").toDF("id", "name", "wiki-title")
+
+	if local:
+		name_wiki_df.show()
+
+	attributes_df = people_df.select("title", "gender", "occupation").toDF("id", "gender", "occupation")
+
+	if local:
+		attributes_df.show()
+
+	wikidata_df = attributes_df.join(name_wiki_df, ['id'], how='inner')
+
+	if local:
+		wikidata_df.show()
+	else:
+		print("="*50)
+		print("Got all attributes from Wikidata!")
+		print("="*50)
+
+	# save the df
+	wikidata_df.write.mode('overwrite').json(os.path.join(LOCAL_PATH, "people_wikidata.json"))
+
+	# woohoo!
+	print("!!!!!!!!!!!!!!!")
+
+
+if __name__ == '__main__':
+	main(**vars(parse_arguments()))
---
+++ src/createDataset/3_filter_people_enwiki.py
@@ -0,0 +1,66 @@
+import os
+from pyspark.sql import *
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from datetime import datetime
+import json
+from params import *
+
+
+def main(**params):
+	params = dict(
+		**params
+	)
+
+	local = params['local']
+
+	if local:
+		LOCAL_PATH = "../../data/"
+		WIKIDATA_PEOPLE = os.path.join(LOCAL_PATH, "people_wikidata_sample.json")
+		ENWIKI = os.path.join(LOCAL_PATH, "enwiki_5000_lines.xml")
+
+	else: # running in the cluster
+		LOCAL_PATH = "hdfs:///user/gullon/"
+		WIKIDATA_PEOPLE = os.path.join(LOCAL_PATH, "people_wikidata.json")
+		ENWIKI = "hdfs:///datasets/enwiki-20191001/enwiki-20191001-pages-articles-multistream.xml"
+
+	spark = SparkSession.builder.getOrCreate()
+	sc = spark.sparkContext
+
+	df = spark.read.format("com.databricks.spark.xml") \
+					.options(rowTag="page") \
+					.load(ENWIKI)
+
+	print(df.printSchema())
+
+	df = df.filter("redirect._title is null")
+	df_enwiki_title = df.select("title", "revision.text._VALUE").toDF("wiki-title", "text")
+
+	if local:
+		print(df_enwiki_title.show())
+	else:
+		print("="*50)
+		print("Got enwiki tite and text")
+		print("="*50)
+
+	# read the wikidata codes
+	df_wikidata = spark.read.json(WIKIDATA_PEOPLE)
+
+	df_biographies = df_wikidata.join(df_enwiki_title,['wiki-title'],how='inner')
+
+	if local:
+		print(df_biographies.show())
+	else:
+		print("="*50)
+		print("Join enwiki and wikidata done")
+		print("="*50)
+
+	# save the df
+	df_biographies.write.mode('overwrite').json(os.path.join(LOCAL_PATH, "biographies_wikipedia.json"))
+
+	# woohoo!
+	print("!!!!!!!!!!!!!!!")
+
+
+if __name__ == '__main__':
+	main(**vars(parse_arguments()))
